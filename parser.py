c51810e198d4b14cff432b94ab61a986c37f1497
Nick <arioxon@gmail.com>
2016-06-25 11:00:11 +0300

    now works for whole pagination

--- parser.py
+++ parser.py
@@ -7,13 +7,12 @@ import re
 
 
 class ByflyIsXponParser(object):
-    PARSER_URL = 'http://byfly.by'
-    STREET = u'Либкнехта'
+    PARSER_URL = 'http://byfly.by{}'
     XPON_COME_SOON = u'Переключение на технологию xPON планируется в ближайшее время'
     XPON_AVAILABLE = u'Техническая возможность подключения по технологии xPON имеется'
     XPON_CHECK_URL = (
         'http://www.byfly.by/gPON-spisok-domov?field_obl_x_value_many_to_one=6&field_street_x_value=&field_ulica_x_value={}&field_number_x_value=&field_sostoynie_x_value_many_to_one=All'
-        )
+    )
 
     FIELD_CLASS_MAP = {
         "region": "views-field-field-obl-x-value",
@@ -27,13 +26,19 @@ class ByflyIsXponParser(object):
         r = requests.get(self.XPON_CHECK_URL.format(street_name))
         soup = bs(r.text, 'html.parser')
 
-        rows = soup.find_all('tr', class_=re.compile(r"(odd|even)"))
-        result = [self._street_connection_data(r) for r in rows]
+        result = []
+        while True:
+            rows = soup.find_all('tr', class_=re.compile(r"(odd|even)"))
+            result += [self._street_connection_data(r) for r in rows]
+            if not soup.find('a', title=u"На следующую страницу", href=True):
+                break
+            next_page_link = soup.find('a', title=u"На следующую страницу", href=True)
+            next_r = requests.get(self.PARSER_URL.format(next_page_link['href']))
+            soup = bs(next_r.text, 'html.parser')
         return result
 
     def _street_connection_data(self, street_row):
         status_data = {}
-
         for k, v in self.FIELD_CLASS_MAP.items():
             el = street_row.find('td', class_=v)
             status_data[k] = el.text.strip()
@@ -42,12 +47,11 @@ class ByflyIsXponParser(object):
 
 def main():
     parser = ByflyIsXponParser()
-    # parser.get_result_dict()
     street_avail_data = parser.check_street(u"Либкнехта")
     for s in street_avail_data:
         if s["status"] == parser.XPON_COME_SOON:
             print("{}, {} - {}".format(s["street"], s["number"],
-                  parser.XPON_COME_SOON))
+                                       parser.XPON_COME_SOON))
 
 if __name__ == '__main__':
     main()
